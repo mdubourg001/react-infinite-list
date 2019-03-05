96ab9b704829b16acb4c24218a453c34b07dea5e
mdubourg <mdubourg@akema.fr>
2019-03-05 22:19:47 +0100

    adding build for netlify deployment

---
+++ build/precache-manifest.298b78fbe370c405554b79b07ab86dff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bb7bd0ad021c342c214b",
+    "url": "/static/js/main.bb7bd0ad.chunk.js"
+  },
+  {
+    "revision": "07c19afc6cb5aec4e76e",
+    "url": "/static/js/2.07c19afc.chunk.js"
+  },
+  {
+    "revision": "43b0df02a6113a6b51f2f619520d8115",
+    "url": "/index.html"
+  }
+];
