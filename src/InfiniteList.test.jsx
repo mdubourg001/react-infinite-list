b2c5d61945286ed4192c65b7112ff0debf44826e
mdubourg <mdubourg@akema.fr>
2019-03-06 14:09:06 +0100

    added some tests

--- src/InfiniteList.test.jsx
+++ src/InfiniteList.test.jsx
@@ -1,11 +1,52 @@
 import React from "react";
-import { render, cleanup } from "react-testing-library";
+import { render, cleanup, fireEvent } from "react-testing-library";
 
 import InfiniteList from "./InfiniteList";
 import mock_fetch from "./mock_data";
 
+let rows = [];
+const limit = 10;
+
 afterEach(cleanup);
 
-it("should render properly and fetch on first render", async => {
-    const { container, getByTagName } = render(<InfiniteList />);
+it("should fetch on first render", () => {
+  const fetchData = jest.fn(offset => {
+    mock_fetch(offset, limit).then(data => {
+      rows = [...rows, ...data];
+      expect(rows.length).toBe(limit);
+    });
+  });
+
+  const { container } = render(
+    <InfiniteList rows={rows} fetchData={fetchData} limit={limit}>
+      {row => <div>{row.name}</div>}
+    </InfiniteList>
+  );
+
+  // should have been called since row was empty on render
+  expect(fetchData).toHaveBeenCalledTimes(1);
+
+  const infiniteListWrapper = container.querySelector("#infinite-list-wrapper");
+
+  fireEvent.scroll(infiniteListWrapper);
+});
+
+it("should fetch again onScroll", () => {
+  rows = [];
+  for (let i = 0; i < 7; i++) {
+    rows.push({ name: `I'm row number ${i}` });
+  }
+
+  const fetchData = jest.fn();
+
+  const { container } = render(
+    <InfiniteList rows={rows} fetchData={fetchData} limit={limit}>
+      {row => <div>{row.name}</div>}
+    </InfiniteList>
+  );
+
+  // should not have been called yet since rows isn't empty
+  expect(fetchData).toHaveBeenCalledTimes(0);
+
+  const infiniteListWrapper = container.querySelector("#infinite-list-wrapper");
 });
--- src/mock_data.js
+++ src/mock_data.js
@@ -1,11 +1,10 @@
-const MOCK_DATA = (offset, limit) => [
-  { name: "Offset " + offset + ", limit " + limit },
-  { name: "Offset " + offset + ", limit " + limit },
-  { name: "Offset " + offset + ", limit " + limit },
-  { name: "Offset " + offset + ", limit " + limit },
-  { name: "Offset " + offset + ", limit " + limit },
-  { name: "Offset " + offset + ", limit " + limit },
-];
+const MOCK_DATA = (offset, limit) => {
+  const data = [];
+  for (let i = 0; i < limit; i++) {
+    data.push(`Offset is ${offset}, limit is ${limit}`);
+  }
+  return data;
+};
 
 export const mock_fetch = (offset, limit) => {
   return Promise.resolve(MOCK_DATA(offset, limit));
